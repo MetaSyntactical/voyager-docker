818119f25f4267d34e94bbd14b72b9a1e77213ca
Daniel Kreuer <d.kreuer@danielkreuer.com>
2023-04-07 18:42:38 +0200

    Add dnsmasq multiple domains and CI

--- files/usr/local/bin/bootstrap-dnsmasq.sh
+++ files/usr/local/bin/bootstrap-dnsmasq.sh
@@ -1,12 +1,12 @@
 #!/command/with-contenv sh
 set -e;
 
-export DNSMASQ_DEFAULT_DOMAIN=${BASE_DOMAIN/#.}
+DNSMASQ_DOMAINS=$BASE_DOMAIN
+export DNSMASQ_DOMAINS
 
 # Replace environment variables in template files
 #envs=`printf '${%s} ' $(sh -c "env|cut -d'=' -f1")`;
-envs='${DNSMASQ_DEFAULT_DOMAIN}'
 for filename in $(find /etc/dnsmasq.d -name '*.tmpl'); do
-    envsubst "$envs" < "$filename" > ${filename: :-5};
+    gomplate --file "$filename" --out "${filename: :-5}";
     rm "$filename";
 done
